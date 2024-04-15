18124f1c3efa4cfafacbb4766015bbf4cd4551e6
dungTnguyen05 <tridung.190705@gmail.com>
2024-04-15 21:27:32 +0930

    completed

---
+++ P2235.cpp
@@ -0,0 +1,14 @@
+/*
+2235. Add Two Integers
+Given two integers num1 and num2, return the sum of the two integers.
+
+Constraints:
+-100 <= num1, num2 <= 100
+*/
+
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+};
