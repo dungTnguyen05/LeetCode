71acb3b36a0bcdae4138ace8f119b16aa1e0f085
dungTnguyen05 <tridung.190705@gmail.com>
2024-04-18 23:01:58 +0930

    completed

---
+++ P1470.cpp
@@ -0,0 +1,25 @@
+/*
+1470. Shuffle the Array
+Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
+Constraints:
+1 <= n <= 500
+nums.length == 2n
+1 <= nums[i] <= 10^3
+*/
+
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> res;
+
+        for (int i = 0; i < n; i++) {
+            res.push_back(nums[i]);
+            res.push_back(nums[i + n]);
+        }
+
+        return res;
+    }
+};
+
