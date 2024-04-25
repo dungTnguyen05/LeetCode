f0dda235ddeea26492d29b5e032696392b0b6fc9
dungTnguyen05 <tridung.190705@gmail.com>
2024-04-25 12:59:07 +0930

    completed

---
+++ P2000.cpp
@@ -0,0 +1,25 @@
+/*
+2000. Reverse Prefix of Word
+Given a 0-indexed string word and a character ch, reverse the segment of word that starts at index 0 and ends at the index of the first occurrence of ch (inclusive). If the character ch does not exist in word, do nothing.
+For example, if word = "abcdefd" and ch = "d", then you should reverse the segment that starts at 0 and ends at 3 (inclusive). The resulting string will be "dcbaefd".
+Return the resulting string.
+
+Constraints:
+1 <= word.length <= 250
+word consists of lowercase English letters.
+ch is a lowercase English letter.
+*/
+
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        for (int i = 0; i < word.size(); i++) {
+            if (word[i] == ch) {
+                reverse(word.begin(), word.begin() + i + 1);
+                break;
+            }
+        }
+
+        return word;
+    }
+};
