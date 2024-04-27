50e840dd42c5380f34be38618c3ff91023aa73a6
dungTnguyen05 <tridung.190705@gmail.com>
2024-04-27 12:47:09 +0930

    completed

---
+++ P2620.js
@@ -0,0 +1,36 @@
+/**
+ * 2620. Counter
+ * Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+ * 
+ * Example 1:
+ * Input: 
+ * n = 10 
+ * ["call","call","call"]
+ * Output: [10,11,12]
+ * Explanation: 
+ * counter() = 10 // The first time counter() is called, it returns n.
+ * counter() = 11 // Returns 1 more than the previous time.
+ * counter() = 12 // Returns 1 more than the previous time.
+ * 
+ * Constraints:
+ * -1000 <= n <= 1000
+ * 0 <= calls.length <= 1000
+ * calls[i] === "call"
+ */
+
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    return function() {
+        return n++;
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
