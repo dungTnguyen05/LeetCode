1c3154265034f0ef01dca08e5857ef1421062121
dungTnguyen05 <tridung.190705@gmail.com>
2024-05-06 16:55:51 +0930

    completed

---
+++ P2348.cpp
@@ -0,0 +1,40 @@
+/*
+2348. Number of Zero-Filled Subarrays
+Given an integer array nums, return the number of subarrays filled with 0.
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+Example 1:
+Input: nums = [1,3,0,0,2,0,0,4]
+Output: 6
+Explanation: 
+There are 4 occurrences of [0] as a subarray.
+There are 2 occurrences of [0,0] as a subarray.
+There is no occurrence of a subarray with a size more than 2 filled with 0. Therefore, we return 6.
+
+Constraints:
+1 <= nums.length <= 10^5
+-10^9 <= nums[i] <= 10^9
+*/
+
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long zero_count = 0;
+        long long res = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0) {
+                zero_count++;
+            }
+
+            else {
+                res += zero_count * (zero_count + 1)/2;
+                zero_count = 0;
+            }
+        }
+
+        res += zero_count * (zero_count + 1)/2;
+
+        return res;
+    }
+};
