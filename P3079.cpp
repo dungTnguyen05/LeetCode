5a4a7cabbdc086fd03ff142e05b7b892f186e93e
dungTnguyen05 <tridung.190705@gmail.com>
2024-07-04 23:18:26 +0930

    completed

---
+++ P3079.cpp
@@ -0,0 +1,41 @@
+/*
+3079. Find the Sum of Encrypted Integers
+You are given an integer array nums containing positive integers. We define a function encrypt such that encrypt(x) replaces every digit in x with the largest digit in x. For example, encrypt(523) = 555 and encrypt(213) = 333.
+Return the sum of encrypted elements.
+
+Example 1:
+Input: nums = [1,2,3]
+Output: 6
+Explanation: The encrypted elements are [1,2,3]. The sum of encrypted elements is 1 + 2 + 3 == 6.
+
+Example 2:
+Input: nums = [10,21,31]
+Output: 66
+Explanation: The encrypted elements are [11,22,33]. The sum of encrypted elements is 11 + 22 + 33 == 66.
+
+Constraints:
+1 <= nums.length <= 50
+1 <= nums[i] <= 1000
+*/
+
+class Solution {
+public:
+    int sumOfEncryptedInt(vector<int>& nums) {
+        int res = 0;
+
+        for (int& num: nums) {
+            int max_digit = 0;
+            int digits = 0;
+
+            while (num != 0) {
+                max_digit = max(max_digit, num % 10);
+                num /= 10;
+                digits++;
+            }
+
+            res += max_digit * stoi(string(digits, '1'));
+        }
+
+        return res;
+    }
+};
